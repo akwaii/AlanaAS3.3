929831b49811b29ea83bd38063de038eb950e2b4
akwaii <alana04.g@gmail.com>
2024-09-18 22:02:20 -0500

    adds constructor, getter and setters to skill class

--- AS3.3/src/Skill.java
+++ AS3.3/src/Skill.java
@@ -1,3 +1,41 @@
 abstract class Skill {
-    
+    private String name, type;
+    private int skillLevel;
+
+    //constructor for skill class
+    public Skill(String name, String type, int skillLevel) {
+        this.name = name;
+        this.type = type;
+        this.skillLevel = skillLevel;
+    }
+
+    //getter for name
+    public String getName(){
+        return name;
+    }
+
+    //setter for name
+    public void setName(String n){
+        this.name = n;
+    }
+
+    //getter for type
+    public String getType(){
+        return type;
+    }
+
+    //setter for type
+    public void setType(String t){
+        this.name = t;
+    }
+
+    //getter for skill level
+    public int getSkillLevel(){
+        return skillLevel;
+    }
+
+    //setter for skill level
+    public void setSkillLevel(int s){
+        this.skillLevel = s;
+    } 
 }
